dc985a8a0684575702e132830a385cbfa852beb4
Carl Haberfeld <chaberfeld@gmail.com>
2015-05-26 12:05:15 -0400

    dustoff

--- js/scripts.js
+++ js/scripts.js
@@ -11,59 +11,59 @@ document.getElementById("per").style.left="0";
 } 
   
  function eduClick() {
-  if (document.getElementById("edu").style.top!=0) {
+  if (document.getElementById("edu").style.top!="700px") {
     document.getElementById("edu").style.top="700px";
     document.getElementById("edu").style.left="3100px";
 
   } else {
-    document.getElementById("edu").style.top="0";
-    document.getElementById("edu").style.left="0";
+    document.getElementById("edu").style.top="0px";
+    document.getElementById("edu").style.left="0px";
   }
 }
  
  function empClick() {
-  if (document.getElementById("emp").style.top!=0) {
-    document.getElementById("emp").style.top="700px";
-    document.getElementById("emp").style.left="3000px";
+  if (document.getElementById("emp").style.top=="700px") {
+    document.getElementById("emp").style.top="0px";
+    document.getElementById("emp").style.left="0px";
 
   } else {
-    document.getElementById("emp").style.top="0";
-    document.getElementById("emp").style.left="0";
+    document.getElementById("emp").style.top="700px";
+    document.getElementById("emp").style.left="3000px";
   }
 } 
 
  
  function projClick() {
-  if (document.getElementById("proj").style.top!=0) {
-    document.getElementById("proj").style.top="700px";
-    document.getElementById("proj").style.left="3200px";
+  if (document.getElementById("proj").style.top=="700px") {
+    document.getElementById("proj").style.top="0px";
+    document.getElementById("proj").style.left="0px";
 
   } else {
-    document.getElementById("proj").style.top="0";
-    document.getElementById("proj").style.left="0";
+    document.getElementById("proj").style.top="700px";
+    document.getElementById("proj").style.left="3200px";
   }
 } 
 
  
  function langClick() {
-  if (document.getElementById("lang").style.top!=0) {
-    document.getElementById("lang").style.top="700px";
-    document.getElementById("lang").style.left="3200px";
+  if (document.getElementById("lang").style.top=="700px") {
+    document.getElementById("lang").style.top="0px";
+    document.getElementById("lang").style.left="0px";
 
   } else {
-    document.getElementById("lang").style.top="0";
-    document.getElementById("lang").style.left="0";
+    document.getElementById("lang").style.top="700px";
+    document.getElementById("lang").style.left="3200px";
   }
 } 
 
  
  function treClick() {
-  if (document.getElementById("tre").style.top!=0) {
-    document.getElementById("tre").style.top="700px";
-    document.getElementById("tre").style.left="3400px";
+  if (document.getElementById("tre").style.top=="700px") {
+    document.getElementById("tre").style.top="0px";
+    document.getElementById("tre").style.left="0px";
 
   } else {
-    document.getElementById("tre").style.top="0";
-    document.getElementById("tre").style.left="0";
+    document.getElementById("tre").style.top="700px";
+    document.getElementById("tre").style.left="3400px";
   }
 } 
