b7662f744fc46d135aac934379b351c60bb87972
Carl Haberfeld <chaberfeld@gmail.com>
2015-07-17 12:27:49 -0400

    notify

--- js/send-email.js
+++ js/send-email.js
@@ -1,6 +1,25 @@
  
 // Send notice to me
 
+// (0) get hostip 
+
+function myIP() {
+    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+
+    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
+    xmlhttp.send();
+
+    hostipInfo = xmlhttp.responseText.split("\n");
+
+    for (i=0; i<hostipInfo.length; i++) {
+        ipAddress = hostipInfo[i].split(":");
+        if ( ipAddress[0] == "IP" ) return ipAddress[1];
+    }
+
+    return false;
+}
+
 // (1) Create a function to log the response from the Mandrill API
   function log(obj) {
     $('#response').text(JSON.stringify(obj));
@@ -10,7 +29,7 @@
   var m = new mandrill.Mandrill('kse6A_PpBO-JMfco3HVodg');
 
   // (3) Send the email!
-  function sendTheMail() {
+  function sendTheMail(mip) {
   
     // Create a variable for the API call parameters
     var params = {
@@ -18,7 +37,7 @@
           "from_email":"someone@somewhere.com",
           "to":[{"email":"chaberfeld@gmail.com"}],
           "subject": "carlResumeSite being viewed",
-          "text": "auto notify"
+          "text": "auto notify from " + mip
       }
     };
 
